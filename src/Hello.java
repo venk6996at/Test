09006da2abaf8ff427bced723931cf0683e85cca
venkat <brvenkat00@gmail.com>
2021-01-15 09:44:23 +0530

    first commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,9 @@ public static void main(String args[]){
 /*
 Use System.out.println() to print on console.
 */
-System.out.println("Hello World 2!");
+System.out.println("Hello World !");
+System.out.println("Hello World 1!");
+
 
 }
 }
